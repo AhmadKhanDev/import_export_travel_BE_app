7ed2da8a7588b481c06b891b8a833b5e678f3d84
AhmadKhanDev <m.ahmadkhan.ucp@gmail.com>
2026-07-02 15:37:02 +0500

    FE and BE started. now starting testing

--- backend/src/main/java/com/marketplace/payment/repository/PaymentRepository.java
+++ backend/src/main/java/com/marketplace/payment/repository/PaymentRepository.java
@@ -1,6 +1,7 @@
 package com.marketplace.payment.repository;
 
 import com.marketplace.payment.entity.Payment;
+import com.marketplace.payment.entity.PaymentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
